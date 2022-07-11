6621d187ab4422937afd44c0d41ac21231eeacee
4720104 <103705583+4720104@users.noreply.github.com>
2022-07-11 13:03:12 +0900

    Update DNA.java

--- src/main/java/DNA.java
+++ src/main/java/DNA.java
@@ -1,3 +1,28 @@
-public class DNA {
 
+/**
+ * 課題12-1
+ * 
+ * @author (4720104 伊藤和真)
+ * @version (2022/7/11)
+ */
+public class DNA {
+    public String str = "";
+
+    public void str (String A){
+        this.str = A;
+    }
+    
+    public String getPair(){
+        
+        this.str = str.replace('A','X');
+        this.str = str.replace('T','Y');
+        this.str = str.replace('X','T');
+        this.str = str.replace('Y','A');
+        this.str = str.replace('C','X');
+        this.str = str.replace('G','Y');
+        this.str = str.replace('X','G');
+        this.str = str.replace('Y','C');
+        
+        return this.str;
+    }
 }
